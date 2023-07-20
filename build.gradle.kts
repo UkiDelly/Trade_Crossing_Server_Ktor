e0ee781ccc429ecb0e034deda3260f0758ad3ca9
Dae Hyeon <daehyeon1999@naver.com>
2023-07-20 10:21:13 +0900

    [UPDATE, ADD] 직렬화/역직렬화 패키지 추가
    - kotlinX 직렬화/역직렬화로 변경
    - MongoDB 커넥션 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,10 @@ val logback_version: String by project
 plugins {
     kotlin("jvm") version "1.9.0"
     id("io.ktor.plugin") version "2.3.2"
+//    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
+    kotlin("plugin.serialization") version "1.5.0"
+
+
 }
 
 group = "ukidelly"
@@ -26,11 +30,13 @@ dependencies {
     implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-swagger-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
-    implementation("io.ktor:ktor-serialization-gson-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-call-logging-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml:$ktor_version")
+    implementation("org.litote.kmongo:kmongo:4.8.0")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("io.ktor:ktor-server-test-host-jvm:2.3.2")
 }
