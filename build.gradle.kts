a77f39409554b70e48a9d627e2d26b5736af098f
UkiDelly <daehyeon1999@naver.com>
2023-08-06 21:03:39 +0900

    [ADD] exposed 패키지의 datetime 처리 패키지 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-core:0.40.1")
     implementation("org.jetbrains.exposed:exposed-dao:0.40.1")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.40.1")
+    implementation("org.jetbrains.exposed:exposed-java-time:0.41.1")
     implementation("org.postgresql:postgresql:42.6.0")
     implementation("com.zaxxer:HikariCP:5.0.1")
 
