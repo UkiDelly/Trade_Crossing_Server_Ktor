2c7ede0391a1b56b35cf990be500e1dc3eaa7a51
UkiDelly <daehyeon1999@naver.com>
2023-08-08 09:33:40 +0900

    [UPDATE] Post, Comment 모델 정리
    - PostTable, PostEntity 분리
    - CommentTable, CommentEntity 분리

--- src/main/kotlin/ukidelly/database/models/post/PostEntity.kt
+++ src/main/kotlin/ukidelly/database/models/post/PostEntity.kt
@@ -3,15 +3,11 @@ package ukidelly.database.models.post
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
-import org.jetbrains.exposed.dao.id.IntIdTable
-import ukidelly.api.v1.post.models.Currency
-import ukidelly.api.v1.post.models.PostCategory
 import ukidelly.database.models.user.UserTable
 
 class PostEntity(
     id: EntityID<Int>
 ) : IntEntity(id) {
-
     companion object : IntEntityClass<PostEntity>(PostTable)
 
     var title by PostTable.title
@@ -24,21 +20,5 @@ class PostEntity(
     var closed by PostTable.closed
     var createdAt by PostTable.createdAt
     var updatedAt by PostTable.updatedAt
-
-}
-
-
-// Table
-object PostTable : IntIdTable("post", "post_id") {
-    val title = varchar("title", 255)
-    val content = text("content")
-    val userId = reference("user_id", UserTable.id)
-    val category = enumerationByName<PostCategory>("category", 100)
-    val currency = enumerationByName<Currency>("currency", 100)
-    val price = integer("price").nullable()
-    val closed = bool("closed")
-    val createdAt = varchar("created_at", 255)
-    val updatedAt = varchar("updated_at", 255)
-
 
 }
