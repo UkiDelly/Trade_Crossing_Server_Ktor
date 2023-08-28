1cd0866cc6515c39b8e4ef7a848b9004f2fba588
UkiDelly <daehyeon1999@naver.com>
2023-08-28 21:38:37 +0900

    [ADD] Comment Routing
    - Comment Routing에 게시글의 모든 댓글을 가져오는 api 테스트 및 성공

--- src/main/kotlin/ukidelly/api/v1/comment/CommentRouting.kt
+++ src/main/kotlin/ukidelly/api/v1/comment/CommentRouting.kt
@@ -1,13 +1,23 @@
 package ukidelly.api.v1.comment
 
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
+import ukidelly.api.v1.comment.service.CommentService
+import ukidelly.systems.models.ResponseDto
 
 fun Route.commentRoutes() {
 
+	val commentService by inject<CommentService>()
+
 	// get all the comments
 	get {
-
+		val comments = commentService.getAllComment(1)
+		call.respond(HttpStatusCode.OK, ResponseDto.Success(comments, "success"))
 	}
+
 
 	// do with a specific comment
 	route("/{commentId}") {
