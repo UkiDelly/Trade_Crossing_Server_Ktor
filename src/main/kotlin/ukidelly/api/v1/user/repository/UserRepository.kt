caba29d5b398a2e7b3f549340df096bbafdac93f
UkiDelly <daehyeon1999@naver.com>
2023-10-06 15:26:23 +0900

    [ADD] native query 처리할 함수 추가

--- src/main/kotlin/ukidelly/api/v1/user/repository/UserRepository.kt
+++ src/main/kotlin/ukidelly/api/v1/user/repository/UserRepository.kt
@@ -13,81 +13,81 @@ import java.util.*
 @Module
 class UserRepository {
 
-	/**
-	 * 이메일 유저 찾기
-	 * @param email 이메일
-	 * @param password 비밀번호
-	 * @return [User]? 유저정보
-	 */
-	suspend fun findEmailUser(email: String): User? {
-		return dbQuery {
-			UserEntity.find {
-				UserTable.loginType eq LoginType.email
-				UserTable.email eq email
-			}.firstOrNull()?.toUser()
-		}
-	}
+    /**
+     * 이메일 유저 찾기
+     * @param email 이메일
+     * @param password 비밀번호
+     * @return [User]? 유저정보
+     */
+    suspend fun findEmailUser(email: String): User? {
+        return dbQuery {
+            UserEntity.find {
+                UserTable.loginType eq LoginType.email
+                UserTable.email eq email
+            }.firstOrNull()?.toUser()
+        }
+    }
 
 
-	/**
-	 * 소셜 로그인
-	 * @param snsId 소셜 ID
-	 * @param email 이메일
-	 * @param loginType 소셜로그인 타입
-	 * @return [User]? 유저정보
-	 */
-	suspend fun findSocialUser(snsId: String, email: String, loginType: LoginType): User? {
-		return dbQuery {
-			UserEntity.find {
-				UserTable.loginType eq loginType
-				UserTable.snsId eq snsId
-				UserTable.email eq email
-			}.firstOrNull()?.toUser()
-		}
-	}
+    /**
+     * 소셜 로그인
+     * @param snsId 소셜 ID
+     * @param email 이메일
+     * @param loginType 소셜로그인 타입
+     * @return [User]? 유저정보
+     */
+    suspend fun findSocialUser(snsId: String, email: String, loginType: LoginType): User? {
+        return dbQuery {
+            UserEntity.find {
+                UserTable.loginType eq loginType
+                UserTable.snsId eq snsId
+                UserTable.email eq email
+            }.firstOrNull()?.toUser()
+        }
+    }
 
 
-	/**
-	 * UUID로 유저 찾기
-	 * @param userId 유저 아이디
-	 * @return [User]? 유저정보
-	 */
-	suspend fun findUserById(userId: UUID): User? {
-		return dbQuery {
-			UserEntity.findById(userId)?.toUser()
-		}
-	}
+    /**
+     * UUID로 유저 찾기
+     * @param userId 유저 아이디
+     * @return [User]? 유저정보
+     */
+    suspend fun findUserById(userId: UUID): User? {
+        return dbQuery {
+            UserEntity.findById(userId)?.toUser()
+        }
+    }
 
 
-	/**
-	 * 이메일로 유저 가입 여부 확인
-	 * @param email 이메일
-	 * @return [User]? 유저정보
-	 */
-	suspend fun findUserByEmail(email: String): User? {
-		return dbQuery {
-			UserEntity.find {
-				UserTable.email eq email
-			}.firstOrNull()?.toUser()
-		}
-	}
+    /**
+     * 이메일로 유저 가입 여부 확인
+     * @param email 이메일
+     * @return [User]? 유저정보
+     */
+    suspend fun findUserByEmail(email: String): User? {
+        return dbQuery {
+            UserEntity.find {
+                UserTable.email eq email
+            }.firstOrNull()?.toUser()
+        }
+    }
 
 
-	/**
-	 * 유저 회원가입
-	 * @param registerRequest 회원가입 요청 Dto
-	 * @return [User] 유저정보
-	 */
-	suspend fun addNewUser(registerRequest: UserRegisterRequest, password: String? = null): User = dbQuery {
-		UserEntity.new {
-			this.snsId = registerRequest.snsId
-			this.email = registerRequest.email
-			this.password = password
-			this.userName = registerRequest.userName
-			this.islandName = registerRequest.islandName
-			this.introduction = registerRequest.introduction
-			this.loginType = registerRequest.loginType
-			this.defaultProfile = registerRequest.profile
-		}.toUser()
-	}
+    /**
+     * 유저 회원가입
+     * @param registerRequest 회원가입 요청 Dto
+     * @return [User] 유저정보
+     */
+    suspend fun addNewUser(registerRequest: UserRegisterRequest, password: String? = null): User = dbQuery {
+        UserEntity.new {
+            this.snsId = registerRequest.snsId
+            this.email = registerRequest.email
+            this.password = password
+            this.userName = registerRequest.userName
+            this.islandName = registerRequest.islandName
+            this.introduction = registerRequest.introduction
+            this.loginType = registerRequest.loginType
+            this.defaultProfile = registerRequest.profile
+        }.toUser()
+    }
 }
--- src/main/kotlin/ukidelly/database/tables/UserTable.kt
+++ src/main/kotlin/ukidelly/database/tables/UserTable.kt
@@ -7,13 +7,13 @@ import ukidelly.systems.models.LoginType
 
 // Table
 object UserTable : UUIDTable("user_info", "user_id") {
-	val snsId = varchar("sns_id", 100)
-	val email = varchar("email", 100)
-	val password = varchar("password", 255).nullable()
-	val defaultProfile = enumerationByName<DefaultProfile>("default_profile", 10)
-	val userName = varchar("user_name", 100)
-	val islandName = varchar("island_name", 100)
-	val introduction = text("introduction")
-	val loginType = enumerationByName<LoginType>("login_type", 10)
+    val snsId = varchar("sns_id", 100)
+    val email = varchar("email", 100)
+    val password = varchar("password", 255).nullable()
+    val defaultProfile = enumerationByName<DefaultProfile>("default_profile", 10)
+    val userName = varchar("user_name", 100)
+    val islandName = varchar("island_name", 100)
+    val introduction = text("introduction")
+    val loginType = enumerationByName<LoginType>("login_type", 10)
 }
 
