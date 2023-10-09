69aac4eba53a5daeda26be7c6c6757f6e6486aff
UkiDelly <daehyeon1999@naver.com>
2023-10-09 22:00:31 +0900

    [UPDATE] Table들 컬럼 타입, 이름 변경
    - UserTable의 password 컬럼을 varchar -> text으로 변경
    - TradePostTable 이름 변경 및 userId 외래키 삭제
    - TadePostCommentTable 이름 변경 및 userId 외래키 삭제
    - FeedTable 이름 변경 및 userId 외래키 삭제

--- src/main/kotlin/ukidelly/database/tables/ImageTable.kt
+++ src/main/kotlin/ukidelly/database/tables/ImageTable.kt
@@ -3,7 +3,6 @@ package ukidelly.database.tables
 import org.jetbrains.exposed.dao.id.IntIdTable
 
 object ImageTable : IntIdTable("images", "id") {
-
-    val url = text("url")
     val postId = reference("post_id", FeedTable.id)
+    val url = text("url")
 }
