10b6d686428637c670ec16e6072fd37f8dc193d2
UkiDelly <daehyeon1999@naver.com>
2023-10-14 20:38:37 +0900

    [UPDATE] StatusPage 에러 수정 - 새로운 ResponseDto.Error의 형식에 맞춰 수정

--- src/main/kotlin/ukidelly/modules/StatusPage.kt
+++ src/main/kotlin/ukidelly/modules/StatusPage.kt
@@ -11,55 +11,56 @@ import kotlinx.serialization.MissingFieldException
 import org.slf4j.LoggerFactory
 import ukidelly.systems.errors.InvalidJwtTokenException
 import ukidelly.systems.errors.PasswordIncorrectException
+import ukidelly.systems.errors.ServerError
 import ukidelly.systems.errors.UserExistException
 import ukidelly.systems.models.ResponseDto
 
 
 @OptIn(ExperimentalSerializationApi::class)
 fun Application.configureStatusPage() {
-	install(StatusPages) {
-		val logger = LoggerFactory.getLogger("ErrorHandle")
-		//
-		exception<InvalidJwtTokenException> { call, exception ->
-			logger.error(exception.cause.toString())
-			call.respond(
-				HttpStatusCode.Unauthorized,
-				ResponseDto.Error(error = "토큰", message = "유효하지 않은 토큰입니다.")
-			)
-		}
-		// Request의 Body에서 필드가 누락되었을 때
-		exception<RequestValidationException> { call, exception ->
-			call.respond(
-				HttpStatusCode.NotAcceptable,
-				ResponseDto.Error(error = exception.reasons, message = "로그인에 실패했습니다.")
-			)
-		}
-		exception<BadRequestException> { call, exception ->
-			logger.error(exception.cause.toString())
-			findException<NullPointerException>(exception)?.let {
-				call.respond(
-					HttpStatusCode.BadRequest,
-					ResponseDto.Error(error = null, message = "null 값이 입력되었습니다.")
-				)
-			}
-		}
-		exception<MissingFieldException> { call, exception ->
-			call.respond(
-				HttpStatusCode.BadRequest,
-				ResponseDto.Error(error = exception.missingFields, message = "필드가 누락되었습니다.")
-			)
-		}
+    install(StatusPages) {
+        val logger = LoggerFactory.getLogger("ErrorHandle")
+        //
+        exception<InvalidJwtTokenException> { call, exception ->
+            logger.error(exception.cause.toString())
+            call.respond(
+                HttpStatusCode.Unauthorized,
+                ResponseDto.Error(error = ServerError.InvalidJwtToken, message = "유효하지 않은 토큰입니다.")
+            )
+        }
+        // Request의 Body에서 필드가 누락되었을 때
+        exception<RequestValidationException> { call, exception ->
+            call.respond(
+                HttpStatusCode.NotAcceptable,
+                ResponseDto.Error(error = ServerError.InvalidField, message = "${exception.reasons}가 유효하지 않습니다.")
+            )
+        }
+        exception<BadRequestException> { call, exception ->
+            logger.error(exception.cause.toString())
+            findException<NullPointerException>(exception)?.let {
+                call.respond(
+                    HttpStatusCode.BadRequest,
+                    ResponseDto.Error(error = ServerError.Null, message = "null 값이 입력되었습니다.")
+                )
+            }
+        }
+        exception<MissingFieldException> { call, exception ->
+            call.respond(
+                HttpStatusCode.BadRequest,
+                ResponseDto.Error(error = ServerError.MissingField, message = "${exception.missingFields} 필드가 누락되었습니다.")
+            )
+        }
 
-		// 커스텀 에러: 유저가 이미 존재
-		exception<UserExistException> { call, e ->
-			call.respond(HttpStatusCode.Conflict, ResponseDto.Error(e.message, "실패"))
-		}
+        // 커스텀 에러: 유저가 이미 존재
+        exception<UserExistException> { call, e ->
+            call.respond(HttpStatusCode.Conflict, ResponseDto.Error(ServerError.UserExist, e.message!!))
+        }
 
-		// 커스턴 에러: 비밀번호 맞지 않음
-		exception<PasswordIncorrectException> { call, e ->
-			call.respond(HttpStatusCode.Forbidden, ResponseDto.Error(e.message, "실패"))
-		}
-	}
+        // 커스턴 에러: 비밀번호 맞지 않음
+        exception<PasswordIncorrectException> { call, e ->
+            call.respond(HttpStatusCode.Forbidden, ResponseDto.Error(ServerError.PasswordIncorrect, e.message!!))
+        }
+    }
 }
 
 /**
@@ -68,6 +69,6 @@ fun Application.configureStatusPage() {
  * @return [T] 타입의 exception이 있으면 해당 exception을 반환하고 없으면 null을 반환합니다.
  */
 private inline fun <reified T> findException(exception: Throwable): T? {
-	return generateSequence(exception) { it.cause }.filterIsInstance<T>()
-		.firstOrNull()
+    return generateSequence(exception) { it.cause }.filterIsInstance<T>()
+        .firstOrNull()
 }
