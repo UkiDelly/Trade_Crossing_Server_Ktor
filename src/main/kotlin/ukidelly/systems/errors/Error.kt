45dce78bfab7afd95dc67eef3940cf6cd066d46e
UkiDelly <daehyeon1999@naver.com>
2023-10-14 20:41:28 +0900

    [ADD] ServerError 생성
    - 새로운 ResponseDto.Error에 쓰일 에러 코드 클래스 생성

---
+++ src/main/kotlin/ukidelly/systems/errors/Error.kt
@@ -0,0 +1,13 @@
+package ukidelly.systems.errors
+
+object ServerError {
+
+    const val InvalidJwtToken = "unauthorized"
+    const val InvalidField = "invalid_field"
+    const val Null = "null"
+    const val MissingField = "missing_field"
+    const val UserExist = "user_exist"
+    const val PasswordIncorrect = "password_incorrect"
+    const val InvalidQueryParameter = "invalie_query_parameter"
+
+}
