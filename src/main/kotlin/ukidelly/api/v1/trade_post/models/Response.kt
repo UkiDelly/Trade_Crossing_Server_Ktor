76d54bb57c26112530a9aa57207da9cb0a7534ac
UkiDelly <daehyeon1999@naver.com>
2023-10-23 10:03:23 +0900

    [UPDATE] TradePostDetailDto 위치 변경
    - TradePostDetailDto를 Response 파일으로 이동

--- src/main/kotlin/ukidelly/api/v1/trade_post/models/Response.kt
+++ src/main/kotlin/ukidelly/api/v1/trade_post/models/Response.kt
@@ -1,6 +1,7 @@
 package ukidelly.api.v1.trade_post.models
 
 import kotlinx.serialization.Serializable
+import ukidelly.api.v1.trade_post.comment.models.TradePostCommentDto
 
 
 @Serializable
@@ -8,4 +9,10 @@ data class LatestTradePostDto(
     val posts: List<TradePostPreview>,
     val currentPage: Int,
     val totalPages: Int
+)
+
+@Serializable
+data class TradePostDetailDto(
+    val post: TradePostDetail,
+    val comments: List<TradePostCommentDto>
 )
