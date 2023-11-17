8be37922dd00b6859b86b8507d53fe06fb2e9dd0
UkiDelly <daehyeon1999@naver.com>
2023-11-17 17:40:36 +0900

    :ambulance: 배포 DB에 연결이 안돼는 문제 해결

--- src/main/kotlin/ukidelly/Application.kt
+++ src/main/kotlin/ukidelly/Application.kt
@@ -4,37 +4,21 @@ import io.ktor.server.application.*
 import io.ktor.server.netty.*
 import ukidelly.database.DataBaseFactory
 import ukidelly.modules.*
-import ukidelly.systems.getServerMode
-import ukidelly.systems.models.ServerMode
+import ukidelly.systems.*
 
 fun main(args: Array<String>): Unit =
     EngineMain.main(args)
 
 
 fun Application.module() {
-
     val serverMode = environment.config.getServerMode()
 
     // DB 연결
     DataBaseFactory.init(
-        databaseUrl = when (serverMode) {
-            ServerMode.dev -> "jdbc:mariadb://localhost:3306/trade_crossing"
-            ServerMode.prod -> environment.config.property("database.url").getString()
-        },
-        driver = when (serverMode) {
-            ServerMode.dev -> "org.mariadb.jdbc.Driver"
-            ServerMode.prod -> "org.postgresql.Driver"
-        },
-
-        user = when (serverMode) {
-            ServerMode.dev -> "root"
-            ServerMode.prod -> environment.config.property("supabase.username").getString()
-        },
-
-        password = when (serverMode) {
-            ServerMode.dev -> "1234"
-            ServerMode.prod -> environment.config.property("database.password").getString()
-        }
+        databaseUrl = environment.config.getDBUrl(serverMode),
+        driver = environment.config.getDriver(serverMode),
+        user = environment.config.getUser(serverMode),
+        password = environment.config.getPassword(serverMode)
     )
 
     // Koin
--- src/main/kotlin/ukidelly/systems/Config.kt
+++ src/main/kotlin/ukidelly/systems/Config.kt
@@ -4,4 +4,30 @@ import io.ktor.server.config.*
 import ukidelly.systems.models.ServerMode
 
 fun ApplicationConfig.getServerMode(): ServerMode =
-	ServerMode.valueOf(this.propertyOrNull("server.mode")?.getString() ?: "dev")
+    ServerMode.valueOf(this.propertyOrNull("server.mode")?.getString() ?: "dev")
+
+
+fun ApplicationConfig.getDBUrl(mode: ServerMode): String =
+    when (mode) {
+        ServerMode.dev -> "jdbc:mariadb://localhost:3306/trade_crossing"
+        ServerMode.prod -> this.property("database.url").getString()
+    }
+
+fun ApplicationConfig.getDriver(mode: ServerMode) =
+    when (mode) {
+        ServerMode.dev -> "org.mariadb.jdbc.Driver"
+        ServerMode.prod -> "org.postgresql.Driver"
+    }
+
+fun ApplicationConfig.getUser(mode: ServerMode): String =
+    when (mode) {
+        ServerMode.dev -> "root"
+        ServerMode.prod -> this.property("database.user").getString()
+    }
+
+
+fun ApplicationConfig.getPassword(mode: ServerMode): String =
+    when (mode) {
+        ServerMode.dev -> "1234"
+        ServerMode.prod -> this.property("database.password").getString()
+    }
