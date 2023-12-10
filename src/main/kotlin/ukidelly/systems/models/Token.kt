183637bc9405226b1fe460b939bc536ab2c6bc7e
UkiDelly <daehyeon1999@naver.com>
2023-12-10 16:12:04 +0900

    👽 update: JWT 토큰 생성 방법 변경
    - AuthRouter 생성

--- src/main/kotlin/ukidelly/systems/models/Token.kt
+++ src/main/kotlin/ukidelly/systems/models/Token.kt
@@ -10,33 +10,57 @@ import java.time.ZoneId
 
 @Serializable
 data class Token(val accessToken: String, val refreshToken: String) {
-	companion object {
-		fun createToken(config: ApplicationConfig, userId: String): Token {
-			val audience = config.property("jwt.audience").getString()
-			val issuer = config.property("jwt.issuer").getString()
-			val secret = config.property("jwt.secret").getString()
+    companion object {
+        fun createToken(config: ApplicationConfig, userId: String): Token {
+            val audience = config.property("jwt.audience").getString()
+            val issuer = config.property("jwt.issuer").getString()
+            val secret = config.property("jwt.secret").getString()
 
-			val accessToken = JWT.create()
-				.withAudience(audience)
-				.withIssuer(issuer)
-				.withSubject(userId)
-				.withClaim("type", "access_token")
-				.withExpiresAt(
-					Instant.now().plusSeconds(60 * 60 * 24).atZone(ZoneId.systemDefault()).toInstant()
-				)
-				.sign(Algorithm.HMAC256(secret))
+            val accessToken = JWT.create()
+                .withAudience(audience)
+                .withIssuer(issuer)
+                .withSubject(userId)
+                .withClaim("type", TokenType.access.name)
+                .withExpiresAt(
+                    Instant.now().plusSeconds(60 * 60 * 24).atZone(ZoneId.systemDefault()).toInstant()
+                )
+                .sign(Algorithm.HMAC256(secret))
 
-			val refreshToken = JWT.create()
-				.withAudience(audience)
-				.withIssuer(issuer)
-				.withSubject(userId)
-				.withClaim("type", "refresh_token")
-				.withExpiresAt(Instant.now().plusSeconds(60 * 60 * 24 * 30).atZone(ZoneId.systemDefault()).toInstant())
-				.sign(Algorithm.HMAC256(secret))
+            val refreshToken = JWT.create()
+                .withAudience(audience)
+                .withIssuer(issuer)
+                .withSubject(userId)
+                .withClaim("type", TokenType.refresh.name)
+                .withExpiresAt(Instant.now().plusSeconds(60 * 60 * 24 * 30).atZone(ZoneId.systemDefault()).toInstant())
+                .sign(Algorithm.HMAC256(secret))
 
-			return Token(accessToken = accessToken, refreshToken = refreshToken)
-		}
-	}
+            return Token(accessToken = accessToken, refreshToken = refreshToken)
+        }
+    }
 }
 
+fun ApplicationConfig.generateToken(userId: String): Token {
+    val audience = this.property("jwt.audience").getString()
+    val issuer = this.property("jwt.issuer").getString()
+    val secret = this.property("jwt.secret").getString()
 
+    val accessToken = JWT.create()
+        .withAudience(audience)
+        .withIssuer(issuer)
+        .withSubject(userId)
+        .withClaim("type", TokenType.access.name)
+        .withExpiresAt(
+            Instant.now().plusSeconds(60 * 60 * 24).atZone(ZoneId.systemDefault()).toInstant()
+        )
+        .sign(Algorithm.HMAC256(secret))
+
+    val refreshToken = JWT.create()
+        .withAudience(audience)
+        .withIssuer(issuer)
+        .withSubject(userId)
+        .withClaim("type", TokenType.refresh.name)
+        .withExpiresAt(Instant.now().plusSeconds(60 * 60 * 24 * 30).atZone(ZoneId.systemDefault()).toInstant())
+        .sign(Algorithm.HMAC256(secret))
+
+    return Token(accessToken = accessToken, refreshToken = refreshToken)
+}
