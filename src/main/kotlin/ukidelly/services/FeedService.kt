1eafda6c1810a25383a3b7a2e703e637a6e6a078
UkiDelly <daehyeon1999@naver.com>
2023-12-28 10:51:39 +0900

    👽 update: 피드 응답 수정
    - 최신피드 응답 수정
    - 피드 응답 수정 피드,댓글 -> 피드

--- src/main/kotlin/ukidelly/services/FeedService.kt
+++ src/main/kotlin/ukidelly/services/FeedService.kt
@@ -1,32 +1,17 @@
 package ukidelly.services
 
 import org.koin.core.annotation.Single
-import ukidelly.dto.responses.FeedCommentDto
-import ukidelly.dto.responses.FeedDto
-import ukidelly.models.FeedPreview
+import ukidelly.dto.responses.LatestFeedDto
+import ukidelly.models.Feed
 import ukidelly.repositories.FeedRepository
 
 
 @Single
 class FeedService(private val feedRepository: FeedRepository) {
-
-
-    suspend fun getLatestPosts(page: Int, size: Int): List<FeedPreview> {
-        return feedRepository.findLatestFeed(size, page)
-
+    suspend fun getLatestPosts(page: Int, size: Int): LatestFeedDto {
+        val feeds = feedRepository.findLatestFeed(size, page)
+        return LatestFeedDto(page, size, feeds.first, feeds.second)
     }
 
-    suspend fun getFeedById(feedId: Int): FeedDto {
-
-        val result = feedRepository.findFeedById(feedId)
-        val commentDtos = mutableListOf<FeedCommentDto>()
-        val comments = result.second
-
-        comments.forEach { parentComment ->
-            val childComments = comments.filter { it.parentComment == parentComment.id }
-            val commentDto = FeedCommentDto(parentComment, childComments)
-            commentDtos.add(commentDto)
-        }
-        return FeedDto(result.first, commentDtos)
-    }
+    suspend fun getFeedById(feedId: Int): Feed = feedRepository.findFeedById(feedId)
 }
