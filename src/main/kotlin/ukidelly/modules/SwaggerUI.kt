1f9398250d434754d8ab398e7eca259e089d98a9
UkiDelly <daehyeon1999@naver.com>
2023-12-31 11:50:32 +0900

    🔧 SwaggerUI 설정

--- src/main/kotlin/ukidelly/modules/SwaggerUI.kt
+++ src/main/kotlin/ukidelly/modules/SwaggerUI.kt
@@ -1,12 +1,29 @@
 package ukidelly.modules
 
+import io.github.smiley4.ktorswaggerui.SwaggerUI
 import io.ktor.server.application.*
-import io.ktor.server.plugins.swagger.*
-import io.ktor.server.routing.*
 
 fun Application.configureSwaggerUI() {
-    routing {
-        swaggerUI(path = "swagger") {
-        }
+  //routing {
+  //    swaggerUI(path = "swagger") {
+  //    }
+  //}
+
+  install(SwaggerUI) {
+    swagger {
+      swaggerUrl = "swagger"
+      forwardRoot = true
     }
+
+    info {
+      title = "Trade Crossing API"
+      version = "1.0.0"
+      description = "Trade Crossing 프로젝트의 API 문서입니다."
+    }
+
+    server {
+      url = "http://localhost:8080"
+      description = "개발 서버"
+    }
+  }
 }
