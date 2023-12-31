bc4a5c4fd6db351e1d1496564272afa9abf9364d
UkiDelly <daehyeon1999@naver.com>
2023-12-31 13:22:06 +0900

    ✨ 거래피드 관련 API 문서 작성 완료

--- src/main/kotlin/ukidelly/api/v2/trade-feed/TradeFeedRouting.kt
+++ src/main/kotlin/ukidelly/api/v2/trade-feed/TradeFeedRouting.kt
@@ -1,74 +1,236 @@
 package ukidelly.api.v2.`trade-feed`
 
+import io.github.smiley4.ktorswaggerui.dsl.resources.delete
+import io.github.smiley4.ktorswaggerui.dsl.resources.get
+import io.github.smiley4.ktorswaggerui.dsl.resources.patch
+import io.github.smiley4.ktorswaggerui.dsl.resources.post
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.request.*
-import io.ktor.server.resources.*
-import io.ktor.server.resources.post
 import io.ktor.server.response.*
-import io.ktor.server.routing.Route
+import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
 import ukidelly.api.v2.`trade-feed`.comments.tradeFeedCommentRoutes
 import ukidelly.dto.requests.CreateTradeFeedRequestDto
 import ukidelly.dto.requests.UpdateTradeFeedRequestDto
+import ukidelly.dto.responses.LatestFeedDto
 import ukidelly.dto.responses.ResponseDto
+import ukidelly.models.TradeFeedDetail
 import ukidelly.modules.getUserId
 import ukidelly.modules.withAuth
 import ukidelly.services.TradeFeedService
+import ukidelly.systems.errors.ServerError
 import ukidelly.systems.models.TokenType
 import java.util.*
 
 fun Route.tradeFeedRouting() {
-    val tradeFeedService by inject<TradeFeedService>()
+  val tradeFeedService by inject<TradeFeedService>()
 
 
-    // 최신 게시글 가져오기
-    get<TradeFeedRoutes> {
-        val feeds = tradeFeedService.getLatestPosts(it.size, it.page)
-        call.respond(HttpStatusCode.OK, ResponseDto.Success(feeds, "성공"))
+  // 최신 게시글 가져오기
+  get<TradeFeedRoutes>({
+    description = "최신 게시글 가져오기"
+    tags = listOf("거래 피드")
+    request {
+      queryParameter<Int>("page") {
+        description = "페이지"
+        example = 1
+        allowEmptyValue = true
+      }
+      queryParameter<Int>("size") {
+        description = "개수"
+        example = 10
+        allowEmptyValue = true
+      }
     }
 
+    response {
+      HttpStatusCode.OK to { body<LatestFeedDto>() }
+    }
+  }) {
+    val feeds = tradeFeedService.getLatestPosts(it.size, it.page)
+    call.respond(HttpStatusCode.OK, ResponseDto.Success(feeds, "성공"))
+  }
 
-    // 게시글 가져오기
-    get<TradeFeedRoutes.FeedId> {
-        val feedData = tradeFeedService.getPost(it.feedId)
-        call.respond(HttpStatusCode.OK, ResponseDto.Success(feedData, "성공"))
+
+  // 게시글 가져오기
+  get<TradeFeedRoutes.FeedId>({
+    description = "게시글 가져오기"
+    tags = listOf("거래 피드")
+
+    request {
+      pathParameter<UUID>("feedId") {
+        description = "게시글 아이디"
+        example = 1
+        required = true
+      }
     }
 
-    withAuth(TokenType.access) {
-
-        // 새 게시물
-        post<TradeFeedRoutes> {
-            val uuid = call.principal<UserIdPrincipal>()!!.name
-            val reqeust = call.receive<CreateTradeFeedRequestDto>()
-            val newPost = tradeFeedService.addNewPost(reqeust, UUID.fromString(uuid))
-            call.respond(HttpStatusCode.Created, ResponseDto.Success(newPost, "성공"))
+    response {
+      HttpStatusCode.OK to { body<TradeFeedDetail>() }
+      HttpStatusCode.NotFound to {
+        body<ResponseDto.Error> {
+          example(
+            "예시",
+            ResponseDto.Error(ServerError.NotExist, "존재하지 않는 게시글입니다.")
+          )
         }
+      }
+    }
+  }) {
+    val feedData = tradeFeedService.getPost(it.feedId)
+    call.respond(HttpStatusCode.OK, ResponseDto.Success(feedData, "성공"))
+  }
 
-        // 좋아요
-        post<TradeFeedRoutes.FeedId.Like> {
-            val feedId = it.feed.feedId
-            val userId = call.getUserId()
-            val result = tradeFeedService.likeFeed(feedId, userId)
-            call.respond(HttpStatusCode.OK, ResponseDto.Success(result, "성공"))
-        }
+  withAuth(TokenType.access) {
 
-        // 수정
-        patch<TradeFeedRoutes.FeedId> {
-            val feedId = it.feedId
-            val request = call.receive<UpdateTradeFeedRequestDto>()
-            val result = tradeFeedService.updateFeed(feedId, request)
-            call.respond(HttpStatusCode.OK, ResponseDto.Success(result, "성공"))
-
+    // 새 게시물
+    post<TradeFeedRoutes>({
+      securitySchemeName = "Access"
+      description = "새 게시물 생성"
+      tags = listOf("거래 피드", "인증")
+      request { body<CreateTradeFeedRequestDto>() }
+      response {
+        HttpStatusCode.Created to { body<TradeFeedDetail>() }
+        HttpStatusCode.NotFound to {
+          body<ResponseDto.Error> {
+            example(
+              "예시",
+              ResponseDto.Error(ServerError.NotExist, "존재하지 않는 게시글입니다.")
+            )
+          }
         }
-        // 삭제
-        delete<TradeFeedRoutes.FeedId> {
-            tradeFeedService.deletePost(it.feedId)
-            call.respond(HttpStatusCode.OK, "성공")
+        HttpStatusCode.Unauthorized to {
+          body<ResponseDto.Error> {
+            example(
+              "예시",
+              ResponseDto.Error(ServerError.UnAuhtorized, "인증이 필요합니다.")
+            )
+          }
         }
+      }
+    }) {
+      val uuid = call.principal<UserIdPrincipal>()!!.name
+      val request = call.receive<CreateTradeFeedRequestDto>()
+      val newPost = tradeFeedService.addNewPost(request, UUID.fromString(uuid))
+      call.respond(HttpStatusCode.Created, ResponseDto.Success(newPost, "성공"))
     }
 
-    // 댓글
-    tradeFeedCommentRoutes()
+    // 좋아요
+    post<TradeFeedRoutes.FeedId.Like>({
+      securitySchemeName = "Access"
+      description = "좋아요"
+      tags = listOf("거래 피드", "인증")
+      request {
+        pathParameter<UUID>("feedId") {
+          description = "게시글 아이디"
+          example = 1
+          required = true
+        }
+      }
+      response {
+        HttpStatusCode.OK to { body<String>() }
+        HttpStatusCode.NotFound to {
+          body<ResponseDto.Error> {
+            example(
+              "예시",
+              ResponseDto.Error(ServerError.NotExist, "존재하지 않는 게시글입니다.")
+            )
+          }
+        }
+        HttpStatusCode.Unauthorized to {
+          body<ResponseDto.Error> {
+            example(
+              "예시",
+              ResponseDto.Error(ServerError.UnAuhtorized, "인증이 필요합니다.")
+            )
+          }
+        }
+      }
+    }) {
+      val feedId = it.feed.feedId
+      val userId = call.getUserId()
+      val result = tradeFeedService.likeFeed(feedId, userId)
+      call.respond(HttpStatusCode.OK, "성공")
+    }
+
+    // 수정
+    patch<TradeFeedRoutes.FeedId>({
+      securitySchemeName = "Access"
+      description = "게시글 수정"
+      tags = listOf("거래 피드", "인증")
+      request {
+        pathParameter<UUID>("feedId") {
+          description = "게시글 아이디"
+          example = 1
+          required = true
+        }
+        body<UpdateTradeFeedRequestDto>()
+      }
+      response {
+        HttpStatusCode.OK to { body<TradeFeedDetail>() }
+        HttpStatusCode.NotFound to {
+          body<ResponseDto.Error> {
+            example(
+              "예시",
+              ResponseDto.Error(ServerError.NotExist, "존재하지 않는 게시글입니다.")
+            )
+          }
+        }
+        HttpStatusCode.Unauthorized to {
+          body<ResponseDto.Error> {
+            example(
+              "예시",
+              ResponseDto.Error(ServerError.UnAuhtorized, "인증이 필요합니다.")
+            )
+          }
+        }
+      }
+    }) {
+      val feedId = it.feedId
+      val request = call.receive<UpdateTradeFeedRequestDto>()
+      val result = tradeFeedService.updateFeed(feedId, request)
+      call.respond(HttpStatusCode.OK, ResponseDto.Success(result, "성공"))
+
+    }
+    // 삭제
+    delete<TradeFeedRoutes.FeedId>({
+      securitySchemeName = "Access"
+      description = "게시글 삭제"
+      tags = listOf("거래 피드", "인증")
+      request {
+        pathParameter<UUID>("feedId") {
+          description = "게시글 아이디"
+          example = 1
+          required = true
+        }
+      }
+      response {
+        HttpStatusCode.OK to { body<String>() }
+        HttpStatusCode.NotFound to {
+          body<ResponseDto.Error> {
+            example(
+              "예시",
+              ResponseDto.Error(ServerError.NotExist, "존재하지 않는 게시글입니다.")
+            )
+          }
+        }
+        HttpStatusCode.Unauthorized to {
+          body<ResponseDto.Error> {
+            example(
+              "예시",
+              ResponseDto.Error(ServerError.UnAuhtorized, "인증이 필요합니다.")
+            )
+          }
+        }
+      }
+    }) {
+      tradeFeedService.deletePost(it.feedId)
+      call.respond(HttpStatusCode.OK, "성공")
+    }
+  }
+
+  // 댓글
+  tradeFeedCommentRoutes()
 }
--- src/main/kotlin/ukidelly/api/v2/trade-feed/comments/TradeFeedCommentRouting.kt
+++ src/main/kotlin/ukidelly/api/v2/trade-feed/comments/TradeFeedCommentRouting.kt
@@ -1,78 +1,215 @@
 package ukidelly.api.v2.`trade-feed`.comments
 
 
+import io.github.smiley4.ktorswaggerui.dsl.resources.delete
+import io.github.smiley4.ktorswaggerui.dsl.resources.get
+import io.github.smiley4.ktorswaggerui.dsl.resources.patch
+import io.github.smiley4.ktorswaggerui.dsl.resources.post
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
-import io.ktor.server.resources.*
-import io.ktor.server.resources.post
-import io.ktor.server.resources.put
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.inject
 import ukidelly.dto.requests.NewCommentRequestDto
 import ukidelly.dto.responses.ResponseDto
+import ukidelly.dto.responses.TradeFeedCommentDto
 import ukidelly.modules.getUserId
 import ukidelly.modules.withAuth
 import ukidelly.services.TradeFeedCommentService
+import ukidelly.systems.errors.ServerError
 import ukidelly.systems.models.TokenType
 
 fun Route.tradeFeedCommentRoutes() {
 
-    val tradeFeedCommentService by inject<TradeFeedCommentService>()
+  val tradeFeedCommentService by inject<TradeFeedCommentService>()
 
+  // 모든 댓글 조회
+  get<TradeFeedCommentRoute>({
+    description = "모든 댓글 조회"
+    tags = listOf("거래 피드", "댓글")
 
-    // 모든 댓글 조회
-    get<TradeFeedCommentRoute> {
-        val id = it.feed.feedId
-        val commentList = tradeFeedCommentService.getAllComment(id)
-        call.respond(HttpStatusCode.OK, ResponseDto.Success(commentList, "성공"))
-
+    request {
+      pathParameter<Int>("feedId") { required = true }
     }
 
-    withAuth(TokenType.access) {
-        // 댓글 추가
-        post<TradeFeedCommentRoute> {
-            val id = it.feed.feedId
-            val request = call.receive<NewCommentRequestDto>()
-            val uuid = call.getUserId()
-            tradeFeedCommentService.addNewComment(id, request.content, uuid)
-            call.respond(HttpStatusCode.OK, "성공")
+    response {
+      HttpStatusCode.OK to { body<List<TradeFeedCommentDto>>() }
+      HttpStatusCode.NotFound to {
+        body<ResponseDto.Error> {
+          example(
+            "예시",
+            ResponseDto.Error(ServerError.NotExist, "존재하지 않는 게시글입니다.")
+          )
         }
+      }
+    }
+  }) {
+    val id = it.feed.feedId
+    val commentList = tradeFeedCommentService.getAllComment(id)
+    call.respond(HttpStatusCode.OK, ResponseDto.Success(commentList, "성공"))
 
+  }
 
-        // 댓글 수정
-        put<TradeFeedCommentRoute.CommentId> {
-            val id = it.commentId
-            val request = call.receive<NewCommentRequestDto>()
-            tradeFeedCommentService.updateComment(id, request.content)
-            call.respond(HttpStatusCode.OK, "성공")
-        }
+  withAuth(TokenType.access) {
+    // 댓글 추가
+    post<TradeFeedCommentRoute>({
+      description = "댓글 추가"
+      tags = listOf("거래 피드", "댓글", "인증")
 
-        // 댓글 삭제
-        delete<TradeFeedCommentRoute.CommentId> {
-            val id = it.commentId
-            tradeFeedCommentService.deleteComment(id)
-            call.respond(HttpStatusCode.OK, "성공")
-        }
+      request {
+        pathParameter<Int>("feedId") { required = true }
+        body<NewCommentRequestDto>()
+      }
 
-
-        // 대댓글 추가
-        post<TradeFeedCommentRoute.CommentId.Reply> {
-            val feedId = it.comment.comments.feed.feedId
-            val commentId = it.comment.commentId
-            val uuid = call.getUserId()
-            val request = call.receive<NewCommentRequestDto>()
-            tradeFeedCommentService.addNewComment(
-                feedId,
-                request.content,
-                uuid,
-                reply = true,
-                parentCommentId = commentId
+      response {
+        HttpStatusCode.OK to { body<String>() }
+        HttpStatusCode.Unauthorized to {
+          body<ResponseDto.Error> {
+            example(
+              "예시",
+              ResponseDto.Error(ServerError.UnAuhtorized, "로그인이 필요합니다.")
             )
-            call.respond(HttpStatusCode.OK, "성공")
+          }
         }
+        HttpStatusCode.NotFound to {
+          body<ResponseDto.Error> {
+            example(
+              "예시",
+              ResponseDto.Error(ServerError.NotExist, "존재하지 않는 게시글입니다.")
+            )
+          }
+        }
+      }
+    }) {
+      val id = it.feed.feedId
+      val request = call.receive<NewCommentRequestDto>()
+      val uuid = call.getUserId()
+      tradeFeedCommentService.addNewComment(id, request.content, uuid)
+      call.respond(HttpStatusCode.OK, "성공")
     }
+
+
+    // 댓글 수정
+    patch<TradeFeedCommentRoute.CommentId>({
+      description = "댓글 수정"
+      tags = listOf("거래 피드", "댓글", "인증")
+
+      request {
+        pathParameter<Int>("commentId") { required = true }
+        body<NewCommentRequestDto>()
+      }
+
+      response {
+        HttpStatusCode.OK to { body<String>() }
+        HttpStatusCode.Unauthorized to {
+          body<ResponseDto.Error> {
+            example(
+              "예시",
+              ResponseDto.Error(ServerError.UnAuhtorized, "로그인이 필요합니다.")
+            )
+          }
+        }
+        HttpStatusCode.NotFound to {
+          body<ResponseDto.Error> {
+            example(
+              "예시",
+              ResponseDto.Error(ServerError.NotExist, "존재하지 않는 게시글입니다.")
+            )
+          }
+        }
+      }
+    }) {
+      val id = it.commentId
+      val request = call.receive<NewCommentRequestDto>()
+      tradeFeedCommentService.updateComment(id, request.content)
+      call.respond(HttpStatusCode.OK, "성공")
+    }
+
+    // 댓글 삭제
+    delete<TradeFeedCommentRoute.CommentId>({
+      description = "댓글 삭제"
+      tags = listOf("거래 피드", "댓글", "인증")
+
+      request {
+        pathParameter<Int>("commentId") { required = true }
+      }
+
+      response {
+        HttpStatusCode.OK to { body<String>() }
+        HttpStatusCode.Unauthorized to {
+          body<ResponseDto.Error> {
+            example(
+              "예시",
+              ResponseDto.Error(ServerError.UnAuhtorized, "본인이 작성한 댓글이 아닙니다.")
+            )
+          }
+        }
+        HttpStatusCode.NotFound to {
+          body<ResponseDto.Error> {
+            example(
+              "예시",
+              ResponseDto.Error(ServerError.NotExist, "존재하지 않는 게시글입니다.")
+            )
+          }
+        }
+      }
+    }) {
+      val id = it.commentId
+      tradeFeedCommentService.deleteComment(id)
+      call.respond(HttpStatusCode.OK, "성공")
+    }
+
+
+    // 대댓글 추가
+    post<TradeFeedCommentRoute.CommentId.Reply>({
+      description = "대댓글 추가"
+      tags = listOf("거래 피드", "댓글", "인증")
+
+      request {
+        pathParameter<Int>("commentId") { required = true }
+        pathParameter<Int>("feedId") { required = true }
+        body<NewCommentRequestDto>()
+      }
+
+      response {
+        HttpStatusCode.OK to { body<String>() }
+        HttpStatusCode.Unauthorized to {
+          body<ResponseDto.Error> {
+            example(
+              "예시",
+              ResponseDto.Error(ServerError.UnAuhtorized, "로그인이 필요합니다.")
+            )
+          }
+        }
+        HttpStatusCode.NotFound to {
+          body<ResponseDto.Error> {
+            example(
+              "게시글 존재 X",
+              ResponseDto.Error(ServerError.NotExist, "존재하지 않는 게시글입니다.")
+            )
+            example(
+              "댓글 존재 X",
+              ResponseDto.Error(ServerError.NotExist, "존재하지 않는 댓글입니다.")
+            )
+          }
+        }
+      }
+    }) {
+      val feedId = it.comment.comments.feed.feedId
+      val commentId = it.comment.commentId
+      val uuid = call.getUserId()
+      val request = call.receive<NewCommentRequestDto>()
+      tradeFeedCommentService.addNewComment(
+        feedId,
+        request.content,
+        uuid,
+        reply = true,
+        parentCommentId = commentId
+      )
+      call.respond(HttpStatusCode.OK, "성공")
+    }
+  }
 
 
 }
